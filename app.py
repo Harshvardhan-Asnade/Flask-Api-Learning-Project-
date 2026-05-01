05566ccfa02f394068bacc9113209b1f9f7f1b6e
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-05-02 03:40:32 +0530

    add TDMB API

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 from flask import Flask,render_template,request
+import requests 
+
 app = Flask(__name__)
-movies = {
-   1: {   "id":1,
-        "title": "Interstellar",
-        "rating": 8.7,
-        "power": 90,
-        "genres": {
-            "Sci-Fi": 95,
-            "Drama": 80
-        }
-    },
-   2: {   "id":2,
-        "title": "Inception",
-        "rating": 8.8,
-        "power": 92,
-        "genres": {
-            "Action": 85,
-            "Sci-Fi": 90
-        }
+
+def fetch_movies():
+    url = "https://api.themoviedb.org/3/trending/movie/week"
+    params={
+        "api_key":"5e4680fa8acf126d39b554e2d1733db0"
     }
-}
+    response=requests.get(url,params=params)
+    data=response.json()
+    return data["results"]
+
 @app.route('/',methods=['GET','POST'])
 def home():
-    filtered_movies=list(movies.values())
+    movies = fetch_movies()
+    filtered_movies=movies
     if request.method=='POST':
         search=request.form.get('search')
         filtered_movies=[]
@@ -35,6 +28,8 @@ def movie_detail(id):
     movie=movies.get(id)
     if movie:
         return render_template('movie_detail.html',movie=movie)
-    return "Movie Not Found"
+    return render_template('404.html')
+
+
 if __name__=='__main__':
     app.run(debug=True,port=3000)
