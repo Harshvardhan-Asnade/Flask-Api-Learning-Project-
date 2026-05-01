1593a18ce4cec60d93fd8d919a13e49ac750ba93
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-05-01 23:33:31 +0530

    passing dis in html using flask

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("index.html")
-@app.route('/about')
-def about():
-    return render_template("about.html")
+    movies = [
+    {
+        "title": "Interstellar",
+        "rating": 8.7,
+        "power": 90,
+        "genres": {
+            "Sci-Fi": 95,
+            "Drama": 80
+        }
+    },
+    {
+        "title": "Inception",
+        "rating": 8.8,
+        "power": 92,
+        "genres": {
+            "Action": 85,
+            "Sci-Fi": 90
+        }
+    }
+]
+    return render_template('index.html', movies=movies)
+@app.route('/movies')
+def movie():
+    return render_template("movies.html")
 if __name__=='__main__':
     app.run(debug=True)
