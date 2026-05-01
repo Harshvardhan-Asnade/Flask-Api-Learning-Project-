5ceb9f1ed0b53b98cadd1336b0cfb5bc70883e5d
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-05-02 00:54:47 +0530

    Changing lis to dis for fast speed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 app = Flask(__name__)
-movies = [
-    {   "id":1,
+movies = {
+   1: {   "id":1,
         "title": "Interstellar",
         "rating": 8.7,
         "power": 90,
@@ -10,7 +10,7 @@ movies = [
             "Drama": 80
         }
     },
-    {   "id":2,
+   2: {   "id":2,
         "title": "Inception",
         "rating": 8.8,
         "power": 92,
@@ -19,15 +19,15 @@ movies = [
             "Sci-Fi": 90
         }
     }
-]
+}
 @app.route('/')
 def home():
     return render_template('index.html', movies=movies)
 @app.route('/movies/<int:id>')
 def movie_detail(id):
-    for movie in movies:
-        if movie["id"]==id:
-            return render_template("movie_detail.html",movie=movie)
+    movie=movies.get(id)
+    if movie:
+        return render_template('movie_detail.html',movie=movie)
     return "Movie Not Found"
 if __name__=='__main__':
     app.run(debug=True)
